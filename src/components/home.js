5a8c073916ac1594ca0c89dd6d3897714700fcc9
Teddy Otieno <teddy.asola@andela.com>
2018-04-19 20:04:17 +0300

    add fonts

--- src/components/home.js
+++ src/components/home.js
@@ -1,34 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div className="u-bg-cover l-full-height l-top-padding">
-            <div className="c-logo">
-                <img src="./../img/logo.svg" alt=""/>
-            </div>
-            <div className="u-center-item">
-                <div className="c-card c-card--home u-beveled-box u-center-item">
-                    <div className="c-card__content l-col-layout
-                ">
-                        <h2>Downtown is Happening</h2>
-                        <p>
-                            Lorem ipsum dolor amet drinking vinegar lumbersexual taiyaki knausgaard leggings hella
-                            microdosing kitsch hell of green juice hot chicken. Ugh hexagon snackwave offal distillery
-                            taiyaki swag twee cliche coloring book irony vegan gastropub. Sartorial four dollar toast
-                        </p>
-
-                        <Link className="c-btn c-btn--primary" to="/chapters"> View Chapters
-                            <span className="u-caret">
-                            <i class="down"></i>
-                        </span>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-
+  return (
+    <div className="u-bg-cover l-full-height l-top-padding">
+      <div className="c-logo">
+        <img src="./../img/logo.svg" alt="" />
+      </div>
+      <div className="u-center-item">
+        <div className="c-card c-card--home u-beveled-box u-center-item">
+          <div className="c-card__content l-col-layout">
+            <h2>Downtown is Happening</h2>
+            <p>
+              Lorem ipsum dolor amet drinking vinegar lumbersexual taiyaki
+              knausgaard leggings hella microdosing kitsch hell of green juice
+              hot chicken. Ugh hexagon snackwave offal distillery taiyaki swag
+              twee cliche coloring book irony vegan gastropub. Sartorial four
+              dollar toast
+            </p>
+            <Link className="c-btn c-btn--primary" to="/chapters">
+              {" "}
+              View Chapters
+              <span className="u-caret">
+                <i class="down" />
+              </span>
+            </Link>Ï
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/story_header.js
+++ src/components/story_header.js
@@ -1,31 +1,32 @@
-import React from 'react';
+import React from "react";
 
 const StoryHeader = () => {
-    return (
-        <div className="c-story-header u-header-bg-cover">
-            <div className="u-beveled-box-bl">
-                <div className="chapter-info">
-                    <div className="chapter-title">
-                        <span className="chapter-marker">01</span>
-                        Downtown in Context
-                        <ul className="story-page-indicator">
-                            <li className="active"></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                        </ul>
-                    </div>
+  return (
+    <div className="c-story-header u-header-bg-cover">
+      <div className="u-beveled-box-bl">
+        <div className="chapter-info">
+          <div className="chapter-title">
+            <span className="chapter-marker">01</span>
+            Downtown in Context
+            <ul className="story-page-indicator">
+              <li className="active" />
+              <li />
+              <li />
+              <li />
+            </ul>
+          </div>
 
-                    <p>
-                        <hr/>
-                        Outine eu hexagon mollit cold-pressed cardigan ethical. Conequat vinyl officia excepteur microdosing air plant mollit aliquip quinoa cliche bespoke meditation messenger bag commodo kinfolk. Hell of esse before they sold out neutra.
-                    </p>
-                </div>
-
-
-            </div>
+          <p>
+            <hr />
+            Outine eu hexagon mollit cold-pressed cardigan ethical. Conequat
+            vinyl officia excepteur microdosing air plant mollit aliquip quinoa
+            cliche bespoke meditation messenger bag commodo kinfolk. Hell of
+            esse before they sold out neutra.
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default StoryHeader;
