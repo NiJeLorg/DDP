dc5c540dfb5bdec4a220f43e0ccd5101e08b9723
Teddy Otieno <teddy.asola@andela.com>
2018-04-27 22:53:25 +0300

    style tweaks

--- src/components/stories.js
+++ src/components/stories.js
@@ -1,39 +1,18 @@
-import React from 'react';
-import StoryNavigator from './story_navigator';
-import GeoMap from './geomap';
-import DataDrawer from './data_drawer';
-import MobileNavStoryMap from './mobile_nav_story_map';
+import React from "react";
+import StoryNavigator from "./story_navigator";
+import GeoMap from "./geomap";
+import DataDrawer from "./data_drawer";
+import MobileNavStoryMap from "./mobile_nav_story_map";
 class Stories extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            mobileView: (window.innerWidth < 500),
-            view: 'story'
-        }
-    }
-
-    handleViewSelect = (e) => {
-        this.setState({ view: e.target.name });
-    }
-    
-    render() {
-        return (
-            <div className="c-stories">
-                { 
-                    this.state.mobileView ? 
-                        <MobileNavStoryMap
-                            view={this.state.view}
-                            handleViewSelect={this.handleViewSelect}
-                        /> : <DataDrawer />
-                }
-                
-                { (!this.state.mobileView || this.state.mobileView && this.state.view === 'story' ) ? <StoryNavigator /> : null }
-
-                { (!this.state.mobileView || this.state.mobileView && this.state.view === 'map' ) ? <GeoMap /> : null }
-                
-            </div>
-        );
-    }
-};
+  render() {
+    return (
+      <div className="c-stories">
+        <DataDrawer />
+        <StoryNavigator />
+        <GeoMap />
+      </div>
+    );
+  }
+}
 
 export default Stories;
--- src/components/stories/sub_stories/education_attainment.js
+++ src/components/stories/sub_stories/education_attainment.js
@@ -6,7 +6,7 @@ import DataBar from './../../charts/data_bar';
 const WorkerEducationAttainment = () => {
     return (
         <div>
-            <h1 className='sub-sub-heading__purple'><span className='ul-yellow-color'>1.</span>EDUCATIONAL ATTAINMENT</h1>
+            <h1 className='sub-sub-heading__purple'><span className='ul-yellow-color'>I.</span>EDUCATIONAL ATTAINMENT</h1>
             <p className='main-text__black'>Outine eu hexagon mollit cold-pressed cardigan ethical. Conequat vinyl officia excepteur microdosing air plant mollit aliquip quinoa cliche bespoke meditation messenger bag commodo kinfolk. Hell of esse before they sold out neutra. </p>
              <div>
                 <span className="chart-header">2016 CENSUS RESIDENTS EDUCATIONAL ATTAINMENT </span>
@@ -16,7 +16,7 @@ const WorkerEducationAttainment = () => {
                 </div>
 
             </div>
-            <p>Something about workers educational attainment Waistcoat normcore trust fund cloud bread asymmetrical
+            <p className='main-text__black'>Something about workers educational attainment Waistcoat normcore trust fund cloud bread asymmetrical
                 neutra. Tacos kickstarter neutra fixie air plant mixtape, raw denim hoodie helvetica offal direct trade.
                 Pug blog vexillologist ugh, paleo hoodie yr jianbing. Air plant activated charcoal hoodie waistcoat
                 lo-fi adaptogen. Hashtag pour-over keytar tofu farm-to-table. Synth brunch squid tattooed, pok pok
